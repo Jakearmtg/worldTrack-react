2a3096be0929abce576ea69a625949c7152515f7
Jakearmtg <joao2000gabriel@gmail.com>
2025-01-15 00:08:29 -0300

    fourth commit

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useReducer,
 } from "react";
 
-const BASE_URL = "https://jakearmtg.github.io/host-cities/";
+const BASE_URL = "https://jakearmtg.github.io/host-cities";
 
 const CitiesContext = createContext();
 
