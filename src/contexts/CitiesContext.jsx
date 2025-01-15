8e593351f47b4d716e50aa271ba0747c7ae2bb52
Jakearmtg <joao2000gabriel@gmail.com>
2025-01-15 00:16:40 -0300

    reverse

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useReducer,
 } from "react";
 
-const BASE_URL = "https://jakearmtg.github.io/host-cities";
+const BASE_URL = "http://localhost:8000";
 
 const CitiesContext = createContext();
 
